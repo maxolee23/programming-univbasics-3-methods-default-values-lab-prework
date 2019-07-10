fb5cea896c9f1981c1d8cb359bcd81204cbfba74
Max Lee <maxolee23@gmail.com>
2019-07-10 02:29:27 +0000

    Automatically backed up by Learn

--- spec/meal_choice_spec.rb
+++ spec/meal_choice_spec.rb
@@ -17,4 +17,7 @@ describe '#meal_choice' do
 end
 
 def meal_choice(veg1, veg2, protein = "meat")
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}"
+end
 end
